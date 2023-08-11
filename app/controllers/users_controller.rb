0a506e40f3286a364225bf699e035a1061d53f11
Robert Bear <robert@Mels-MacBook-Pro.local>
2023-08-11 01:07:30 -0700

    Add Basic User routes to create user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+  def index
+		@users = User.all
+  end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		if @user.destroy
+			flash[:notice] = "User destroyed"
+		else
+			flash[:error] = "User unable to destory"
+		end
+		redirect_to users_path
+	end
+
+  def new
+		@user = User.new
+  end
+	
+	def create
+		@user = User.new(user_params)
+    if @user.save
+			flash[:notice] = "User created"
+      redirect_to @user
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+			flash[:error] = "User invalid"
+      render "new"
+    end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+    if @user.update(user_params)
+			flash[:notice] = "User modified"
+      redirect_to @user
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+			flash[:error] = "User invalid"
+      render "edit"
+    end
+	end
+
+
+	private
+	
+	def user_params
+		params.require(:user).permit(:name, :debt)
+	end
+end
