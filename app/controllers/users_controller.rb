e2c960efe2b2126e377d0bbd677baba2f95b3874
Robert Bear <robert@Mels-MacBook-Pro.local>
2023-08-20 16:26:12 -0700

    Added unit and system tests for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,60 +1,62 @@
+# frozen_string_literal: true
+
+# UsersController hanldes user-related actions such as creation, deletion, and modications.
 class UsersController < ApplicationController
   def index
-		@users = User.all
+    @users = User.all
   end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-	def destroy
-		@user = User.find(params[:id])
-		if @user.destroy
-			flash[:notice] = "User destroyed"
-		else
-			flash[:error] = "User unable to destory"
-		end
-		redirect_to users_path
-	end
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = 'User destroyed'
+    else
+      flash[:error] = 'User unable to destory'
+    end
+    redirect_to users_path
+  end
 
   def new
-		@user = User.new
+    @user = User.new
   end
-	
-	def create
-		@user = User.new(user_params)
+
+  def create
+    @user = User.new(user_params)
     if @user.save
-			flash[:notice] = "User created"
+      flash[:success] = 'User created'
       redirect_to @user
     else
       # This line overrides the default rendering behavior, which
       # would have been to render the "create" view.
-			flash[:error] = "User invalid"
-      render "new"
+      flash[:error] = 'User invalid'
+      render 'new'
     end
-	end
+  end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def update
-		@user = User.find(params[:id])
+  def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-			flash[:notice] = "User modified"
+      flash[:success] = 'User modified'
       redirect_to @user
     else
       # This line overrides the default rendering behavior, which
       # would have been to render the "create" view.
-			flash[:error] = "User invalid"
-      render "edit"
+      flash[:error] = 'User invalid'
+      render 'edit'
     end
-	end
+  end
 
+  private
 
-	private
-	
-	def user_params
-		params.require(:user).permit(:name, :debt)
-	end
+  def user_params
+    params.require(:user).permit(:name, :debt)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# Represents a user in the system.
 class User < ActiveRecord::Base
+  validates :name, presence: true
 end
